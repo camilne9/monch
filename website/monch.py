6d81fa35f93c1e2ea6f1d8902de2aa0c2302b974
camilne9 <ubuntu@ide.ide.454fadb4-b1bd-4052-9f06-331c838e529a.svc.cluster.local>
2019-12-04 05:31:50 +0000

    working on getting user input for time

--- website/monch.py
+++ website/monch.py
@@ -13,3 +13,21 @@ test = str(now.hour) + ":" + str(now.minute)
 # houses = db.execute(f"SELECT * FROM generic_day WHERE {time} IS end_time")
 
 print(f"{test} \n {now}")
+
+@app.route("/dhallranks", methods=["GET", "POST"])
+@login_required
+def dhallranks():
+    valid_hours = list(range(1, 13))
+    valid_minutes = list(range(0, 60))
+    if request.method == "POST":
+        hour = request.form.get("Hour")
+        minute = request.form.get("Minute")
+        if request.form.get("Hour") == none or request.form.get("Minute") == none or request.form.get("AM/PM") == none:
+            return apology("Not a valid time")
+        if request.form.get("") == "AM":
+            value = 60*hour + minute
+        else:
+            value = 60 * (hour + 12) + minute
+        return render_template("open.html", value=value)
+    else:
+        return render_template("arbitrary_time.html", valid_hours = valid_hours, valid_minutes = valid_minutes)
