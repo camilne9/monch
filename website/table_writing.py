0b7c414f3be5c560027a96ae0db706e45763732a
camilne9 <ubuntu@ide.ide.454fadb4-b1bd-4052-9f06-331c838e529a.svc.cluster.local>
2019-12-04 03:52:30 +0000

    Created tables

--- website/table_writing.py
+++ website/table_writing.py
@@ -14,8 +14,23 @@ data = csv.DictReader(file)
 
 for row in data:
     # we loop through all of the rows in the data
-    db.execute("INSERT INTO students (start_time, end_time, house, restriction_id) VALUES(?, ?, ?, ?)",
+    db.execute("INSERT INTO generic_day (start_time, end_time, house, restriction_id) VALUES(?, ?, ?, ?)",
                    row["start_time"], row["end_time"], row["house"], row["restriction_id"])
+    # we put all of the information from the csv into the SQL table
+
+file.close()
+# We make sure to close the file.
+
+file = open("restrictions.csv", "r")
+# we take the csv file with the information about the restrictions
+# so we open it to read it to memory.
+restrictions = csv.DictReader(file)
+# We read the csv file in as a dictionary.
+
+for other_row in restrictions:
+    # we loop through all of the rows in the data
+    db.execute("INSERT INTO restrictions (restriction_id, open_to, house_in_question) VALUES(?, ?, ?)",
+                   other_row["restriction_id"], other_row["open_to"], other_row["house_in_question"])
     # we put all of the information from the csv into the SQL table
 
 file.close()
