ad38a94b7e165fc338166c7196553f00dde73c57
camilne9 <ubuntu@ide.ide.454fadb4-b1bd-4052-9f06-331c838e529a.svc.cluster.local>
2019-12-04 20:23:17 +0000

    made all_days sql table

--- website/table_writing.py
+++ website/table_writing.py
@@ -6,24 +6,24 @@ from cs50 import SQL
 db = SQL("sqlite:///monch.db")
 # This will allow us to load the information from the csv to the sql database.
 
-db.execute("DELETE FROM generic_day")
+#db.execute("DELETE FROM generic_day")
 db.execute("DELETE FROM restrictions")
-db.execute("DELETE FROM new_generic_day")
+#db.execute("DELETE FROM new_generic_day")
 
-file = open("generic_day.csv", "r")
-# we take the csv file with the information about the dhall hours
-# so we open it to read it to memory.
-data = csv.DictReader(file)
-# We read the csv file in as a dictionary.
+# file = open("generic_day.csv", "r")
+# # we take the csv file with the information about the dhall hours
+# # so we open it to read it to memory.
+# data = csv.DictReader(file)
+# # We read the csv file in as a dictionary.
 
-for row in data:
-    # we loop through all of the rows in the data
-    db.execute("INSERT INTO generic_day (start_time, end_time, house, restriction_id) VALUES(?, ?, ?, ?)",
-                   row["start_time"], row["end_time"], row["house"], row["restriction_id"])
-    # we put all of the information from the csv into the SQL table
+# for row in data:
+#     # we loop through all of the rows in the data
+#     db.execute("INSERT INTO generic_day (start_time, end_time, house, restriction_id) VALUES(?, ?, ?, ?)",
+#                   row["start_time"], row["end_time"], row["house"], row["restriction_id"])
+#     # we put all of the information from the csv into the SQL table
 
-file.close()
-# We make sure to close the file.
+# file.close()
+# # We make sure to close the file.
 
 file = open("restrictions.csv", "r")
 # we take the csv file with the information about the restrictions
@@ -40,7 +40,22 @@ for other_row in restrictions:
 file.close()
 # We make sure to close the file.
 
-file = open("new_generic_day.csv", "r")
+# file = open("new_generic_day.csv", "r")
+# # we take the csv file with the information about the dhall hours
+# # so we open it to read it to memory.
+# data = csv.DictReader(file)
+# # We read the csv file in as a dictionary.
+
+# for row in data:
+#     # we loop through all of the rows in the data
+#     db.execute("INSERT INTO new_generic_day (start_time, end_time, house, restriction_id) VALUES(?, ?, ?, ?)",
+#                   row["start_time"], row["end_time"], row["house"], row["restriction_id"])
+#     # we put all of the information from the csv into the SQL table
+
+# file.close()
+# # We make sure to close the file.
+
+file = open("all_days.csv", "r")
 # we take the csv file with the information about the dhall hours
 # so we open it to read it to memory.
 data = csv.DictReader(file)
@@ -48,8 +63,8 @@ data = csv.DictReader(file)
 
 for row in data:
     # we loop through all of the rows in the data
-    db.execute("INSERT INTO new_generic_day (start_time, end_time, house, restriction_id) VALUES(?, ?, ?, ?)",
-                   row["start_time"], row["end_time"], row["house"], row["restriction_id"])
+    db.execute("INSERT INTO all_days (start_time, end_time, house, monday, tuesday, wednesday, thursday, friday, saturday, sunday) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                   row["start_time"], row["end_time"], row["house"], row["monday"], row["tuesday"], row["wednesday"], row["thursday"], row["friday"], row["saturday"], row["sunday"])
     # we put all of the information from the csv into the SQL table
 
 file.close()
