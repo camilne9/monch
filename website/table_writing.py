bd1b36dc739774151b1c40ca104eb7a9838cb810
camilne9 <ubuntu@ide.ide.454fadb4-b1bd-4052-9f06-331c838e529a.svc.cluster.local>
2019-12-04 03:21:55 +0000

    writing tables

---
+++ website/table_writing.py
@@ -0,0 +1,22 @@
+import csv
+from sys import exit
+from cs50 import SQL
+# First we load the necessary packages.
+
+db = SQL("sqlite:///monch.db")
+# This will allow us to load the information from the csv to the sql database.
+
+file = open("generic_day.csv", "r")
+# we take the csv file with the information about the dhall hours
+# so we open it to read it to memory.
+data = csv.DictReader(file)
+# We read the csv file in as a dictionary.
+
+for row in data:
+    # we loop through all of the rows in the data
+    db.execute("INSERT INTO students (start_time, end_time, house, restriction_id) VALUES(?, ?, ?, ?)",
+                   row["start_time"], row["end_time"], row["house"], row["restriction_id"])
+    # we put all of the information from the csv into the SQL table
+
+file.close()
+# We make sure to close the file.
